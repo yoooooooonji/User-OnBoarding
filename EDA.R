24b90a9fdce2394f87c5709a33ca4c2528c3133b
Yoonji <yoonji.noh90@gmail.com>
2023-10-26 19:19:39 +0900

    modeling

--- EDA.R
+++ EDA.R
@@ -13,15 +13,15 @@ pkg <- c("readr", "dplyr", "tidytext", "tidyverse", "lubridate", "reshape2", "ps
 ipak(pkg)
 
 ##########################################################################################################################################################
-# data load
-data <- read_excel("/Users/yj.noh/Documents/GitHub/prj_on_boarding/data_filtered_both.xlsx")
-#data <- read_excel("/Users/yj.noh/Desktop/on_boarding_data.xlsx")
+# 
+# 전체 
+data <- read_excel("/Users/yj.noh/Desktop/on_boarding_data.xlsx")
+
 head(data)
 str(data)
+dim(data)
 
 data <- data  %>% mutate(is_recom = ifelse (is_recom == TRUE, 1, 0))
-
-dim(data) 
 
 
 data[c("birth", "delivery_method", "insurance_type", "is_recom", "gender", "day_cnt", "avg_cnt", "outcome")] %>% 
@@ -41,4 +41,20 @@ data[c("birth", "delivery_method", "insurance_type", "is_recom", "gender", "day_
 
 
 
+# bike만
+bike <- read_excel("/Users/yj.noh/Documents/GitHub/prj_on_boarding/data_filtered_both.xlsx")
 
+bike[c("birth", "delivery_method", "insurance_type", "is_recom", "gender", "day_cnt", "avg_cnt", "outcome")] %>% 
+  tbl_summary(
+    by = outcome,
+   type = list(
+    day_cnt ~ "continuous2",
+    avg_cnt ~ "continuous2"
+   ),  
+    statistic = all_continuous() ~ c("{mean} ({sd})", "{min}, {max}"),
+    missing_text = "(Missing value)", 
+    digits = list(all_continuous() ~ 2, all_categorical() ~ c(0, 1))
+  ) %>%
+  add_overall() %>%
+  add_p(pvalue_fun = ~style_pvalue(., digits = 3)) %>%
+  bold_labels()
