d0bbd0fc7afd0324cf4ae350487fcd04d42dfc1d
Yoonji <yoonji.noh90@gmail.com>
2023-11-01 18:33:04 +0900

    modeling

--- EDA.R
+++ EDA.R
@@ -41,7 +41,7 @@ data[c("birth", "delivery_method", "insurance_type", "is_recom", "gender", "avg_
   add_p(pvalue_fun = ~style_pvalue(., digits = 3)) %>%
   bold_labels()
 
-
+##########################################################################################################################################################
 
 # bike만
 bike <- read_excel("/Users/yj.noh/Documents/GitHub/prj_on_boarding/data_filtered_both.xlsx")
@@ -54,6 +54,34 @@ bike[c("birth", "delivery_method", "insurance_type", "is_recom", "gender", "avg_
     active_days ~ "continuous2",
     avg_distance ~ "continuous2",
     avg_fee ~ "continuous2"
+   ),  
+    statistic = all_continuous() ~ c("{mean} ({sd})", "{min}, {max}"),
+    missing_text = "(Missing value)", 
+    digits = list(all_continuous() ~ 2, all_categorical() ~ c(0, 1))
+  ) %>%
+  add_overall() %>%
+  add_p(pvalue_fun = ~style_pvalue(., digits = 3)) %>%
+  bold_labels()
+
+
+##########################################################################################################################################################
+# RFM  
+data <- read_excel("/Users/yj.noh/Desktop/rfm_data.xls")
+
+head(data)
+str(data)
+dim(data)
+
+data <- data  %>% filter(!is.na(working_cycle))
+data$work_start_week<- factor(data$work_start_week, levels = c('8W1D', '8W2D', '8W3D', '8W4D', '9W1D', '9W2D', '9W3D', '9W4D'
+,'10W1D',  '10W2D', '10W3D', '10W4D','10W5D'))
+
+df = subset(data, select = -c(brms_rider_id))
+df %>%  
+  tbl_summary(
+    by = work_start_week,
+   type = list(
+    all_continuous() ~ "continuous2"
    ),  
     statistic = all_continuous() ~ c("{mean} ({sd})", "{min}, {max}"),
     missing_text = "(Missing value)", 
